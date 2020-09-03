2719b098e308ac6fc661106ec2b88a4e373c396d
karlkunze <kunzx37@gmail.com>
2020-09-03 18:59:35 -0400

    added code for subset envelopes for DH germination sampling sunday

--- Envelope_Prep.R
+++ Envelope_Prep.R
@@ -1,48 +1,15 @@
 library(readxl)
 setwd('C:/Users/Karl/Google Drive/Grad School_/Field 2020/Germination_assays')
-masses = data.frame(read_excel(path = 'C:/Users/Karl/Google Drive/Grad School_/Field 2020/Germination_assays/PHSsubsample_PMdates_MassCollected.xlsx', sheet = 'MassCollected'))
 
-library(ggplot2)
-ggplot(data = masses, aes(x = MassGrams))+geom_histogram()
-masses$numkernels = masses$MassGrams/0.05
-
-masses$assays = trunc(masses$numkernels/30)
-ggplot(data = masses, aes(x = assays))+geom_histogram()
-sum(ifelse(masses$assays<6,masses$PLOT,0))
-ifelse(masses$assays<6,masses$PLOT,NA)
-
-Helfer = subset(masses,masses$PLOT>1999& masses$PLOT<2449)
-ggplot(Helfer, aes(x = MassGrams))+geom_histogram()
-Helfer$massAfterMalt = Helfer$MassGrams - 5
-Helfer$assaysAfterMalt = trunc(Helfer$massAfterMalt/0.05/30)
-Helfer$assaysAfterMalt
-
-ggplot(Helfer, aes(x = assaysAfterMalt))+geom_histogram()
-
-CaldwellGGS = subset(masses,masses$PLOT>1000& masses$PLOT<1449)
-ggplot(CaldwellGGS, aes(x = MassGrams))+geom_histogram()
-CaldwellGGS$massAfterMalt = CaldwellGGS$MassGrams - 5
-CaldwellGGS$assaysAfterMalt = trunc(CaldwellGGS$massAfterMalt/0.05/30)
-CaldwellGGS$assaysAfterMalt
-
-ggplot(CaldwellGGS, aes(x = assaysAfterMalt))+geom_histogram()
-
-library(gridExtra)
-grid.arrange(ggplot(Helfer, aes(x = assaysAfterMalt))+geom_histogram()+
-               xlim(-3,35)+ggtitle('Helfer GGS'),
-             ggplot(CaldwellGGS, aes(x = assaysAfterMalt))+geom_histogram()+
-               xlim(-3,35)+ggtitle('caldwell'))
-
-# Take the MQ sample for micro malting from HELFER
-# need 6 packets of everything, and 2 extra for malting quality for 
-# the GGS population of lines. 
 packetsList = data.frame(read_excel(path = 'GGS_EntriesandPlots.xlsx'))
+
 set.seed(1)
 packetsList$Sort = runif(length(packetsList$PLOT),0,1)
 
 packetsList = packetsList[order(packetsList$Sort),]
 
 packetsList$SampleGroup = c(rep('a',1122/2),rep('b',1122/2))
+ 
 packetsList$GroupOrder = paste(packetsList$SampleGroup,packetsList$PLOT)
 packetsList = packetsList[order(packetsList$GroupOrder),]
 packetsList$number = c(1:561,1:561)
@@ -54,11 +21,32 @@ packetsListFull$Purpose = '30kernel'
 
 write.csv(packetsListFull,file = 'PacketsToPrint30kernelAssays.csv')
 
-packetsListMQ = subset(packetsList,packetsList$PLOT %in% c(2001:2448))
-packetsListMQ = rbind(packetsListMQ,packetsListMQ)
-packetsListMQ = packetsListMQ[order(packetsListMQ$GroupOrder),]
-packetsListMQ$Purpose = 'MQ 2.5g'
+DHList_Snyder= data.frame(read_excel(path = 'DH_2020_germination_assay_list.xlsx',sheet='Snyder'))
+DHList_Ketola= data.frame(read_excel(path = 'DH_2020_germination_assay_list.xlsx',sheet='Ketola'))
+DHList_Ketola_PHS= data.frame(read_excel(path = 'DH_2020_germination_assay_list.xlsx',sheet='PHS_list_Ketola'))
+DHList_Ketola<-merge(DHList_Ketola,DHList_Ketola_PHS,by=c('PLOT','Entry'),all=TRUE)
+DHList_Ketola
+DHList<-merge(DHList_Snyder,DHList_Ketola,by=c('Entry','Check','Source','Pedigree','Facultative_Spring_planted'),all = TRUE)
+library(dplyr)
+DHList$Thresh_sample.y
 
+DHList_Check<- DHList[which(DHList$Check=="C" & DHList$),]
+nrow(DHList_Check)
+DHList_Sprout<- DHList[which(DHList$Sprout=="Y"),]
+
+nrow(DHList_Check_Sprout)
+View(DHList_Check_Sprout)
+DHList_Other<-DHList[-which(DHList$Check=="C" | DHList$Sprout=="Y"),]  
+nrow(DHList_Other)
+
+
+typeof(DHList$Thresh_sample.x)
+
+View(DHList[,c('Entry','Thresh_sample.x','Thresh_sample.y')])
+by_cyl <- DHList %>% group_by(Pedigree)
+c<-sample_frac(by_cyl,0.5)
+table(c$Pedigree)
+table(DHList$Pedigree)
 # write.csv(packetsListMQ, file = 'Data/GerminationTests_2020/PrepForAssays/MaltingQualityPackets.csv')
 
 
