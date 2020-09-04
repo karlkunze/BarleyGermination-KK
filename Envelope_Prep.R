0ee4598bf366548f56fec95b92fe635ad29409e9
karlkunze <kunzx37@gmail.com>
2020-09-04 12:46:07 -0400

    subsetted checks and non-checks first, will keep all even if they are not at both locations
    subsetted out all lines with PHS data, will keep all even if they are not at both locations
    they are also many of the facultative
    
    remaining subset to sample from consists of lines from both locations
    there were 300 common lines in this sample made a random sample of 120

--- Envelope_Prep.R
+++ Envelope_Prep.R
@@ -23,16 +23,65 @@ write.csv(packetsListFull,file = 'PacketsToPrint30kernelAssays.csv')
 
 DHList_Snyder= data.frame(read_excel(path = 'DH_2020_germination_assay_list.xlsx',sheet='Snyder'))
 DHList_Ketola= data.frame(read_excel(path = 'DH_2020_germination_assay_list.xlsx',sheet='Ketola'))
+
 DHList_Ketola_PHS= data.frame(read_excel(path = 'DH_2020_germination_assay_list.xlsx',sheet='PHS_list_Ketola'))
 DHList_Ketola<-merge(DHList_Ketola,DHList_Ketola_PHS,by=c('PLOT','Entry'),all=TRUE)
-DHList_Ketola
-DHList<-merge(DHList_Snyder,DHList_Ketola,by=c('Entry','Check','Source','Pedigree','Facultative_Spring_planted'),all = TRUE)
-library(dplyr)
-DHList$Thresh_sample.y
+nrow(DHList_Snyder)
+nrow(DHList_Ketola)
+DHList_Ketola_Check<-DHList_Ketola[which(DHList_Ketola$Check=="C"),]
+nrow(DHList_Ketola_Check)
 
-DHList_Check<- DHList[which(DHList$Check=="C" & DHList$),]
-nrow(DHList_Check)
-DHList_Sprout<- DHList[which(DHList$Sprout=="Y"),]
+
+DHList_Ketola<-DHList_Ketola[which(DHList_Ketola$Check=="B"),]
+nrow(DHList_Ketola)
+DHList_Snyder_Check<-DHList_Snyder[which(DHList_Snyder$Check=="C"),]
+nrow(DHList_Snyder_Check)
+
+DHList_Snyder<-DHList_Snyder[which(DHList_Snyder$Check=="B"),]
+head(DHList_Snyder)
+
+DHList_Snyder_Check$Check_ID<-substr(DHList_Snyder_Check$PLOT,3,5)
+DHList_Ketola_Check$Check_ID<-substr(DHList_Ketola_Check$PLOT,3,5)
+
+DHList_Check<-merge(DHList_Snyder_Check,DHList_Ketola_Check,by=c('Entry','Check','Source','Pedigree','Facultative_Spring_planted','Check_ID'),all = TRUE)
+
+nrow(DHList_Snyder)
+nrow(DHList_Ketola)
+DHList<-merge(DHList_Snyder,DHList_Ketola,by=c('Entry','Check','Source','Pedigree','Facultative_Spring_planted'),all = TRUE)
+write.csv(DHList, file="Dhlist_0.csv")
+
+
+DHList_PHS<-DHList[which(DHList$Sprout=="Y"),]
+nrow(DHList_PHS)
+DHList<-DHList[-which(DHList$Sprout=="Y"),]
+
+DHlist_common<-DHList[which(DHList$Thresh_sample.x=="Y"),]
+
+nrow(DHList_Check)/(0.5*(nrow(DHList) +nrow(DHList_PHS)))
+nrow(DHList_PHS)
+
+0.5*nrow(DHList)
+nrow(DHlist_common)
+nrow(DHlist_common)+nrow(DHList_PHS)
+250-nrow(DHList_PHS)-nrow(DHList_Check)
+
+table(DHList$Pedigree,DHList$Facultative_Spring_planted)
+table(DHlist_common$Pedigree,DHlist_common$Facultative_Spring_planted)
+#120 from the common list need to be selected
+
+c1<-nrow(DHList_PHS)
+c2<-nrow(DHList_Check)
+
+
+
+nrow(DHList)
+nrow(DHlist_common)
+value<-120/nrow(DHlist_common)
+set.seed(1)
+library(dplyr)
+by_cyl <- DHlist_common %>% group_by(Pedigree)
+Common_sample<-sample_frac(by_cyl,value)
+nrow(Common_sample)
 
 nrow(DHList_Check_Sprout)
 View(DHList_Check_Sprout)
@@ -43,7 +92,7 @@ nrow(DHList_Other)
 typeof(DHList$Thresh_sample.x)
 
 View(DHList[,c('Entry','Thresh_sample.x','Thresh_sample.y')])
-by_cyl <- DHList %>% group_by(Pedigree)
+
 c<-sample_frac(by_cyl,0.5)
 table(c$Pedigree)
 table(DHList$Pedigree)
