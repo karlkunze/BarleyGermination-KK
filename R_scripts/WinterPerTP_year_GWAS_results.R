878e8bcecca1e8302ee9b2a2880a199424f47e97
karlkunze <khk44@cornell.edu>
2022-01-11 12:24:25 -0500

    added GWAS plot

--- R_scripts/WinterPerTP_year_GWAS_results.R
+++ R_scripts/WinterPerTP_year_GWAS_results.R
@@ -1,1 +1,22 @@
 #graphing outputs from WinterTPGWAS
+load("data/WinterPerTPGWAS.Rdata")
+chrTable = c(785, 1534, 1187,  760, 1348,  823, 1334,16 )
+chrLabel = c(1:7, 'UN')
+winterOrdinalBreaks = c(chrTable[1]/2)
+WinterChrLines = c(785)
+for (i in 2:8){
+  winterOrdinalBreaks[i] = sum(chrTable[1:i-1])+chrTable[i]/2
+  WinterChrLines[i] = sum(chrTable[1:i])
+}
+
+
+
+WinterPerTPGWAS %>% ggplot(aes(ordinal, log10PVal, color = TP, shape = year))+geom_point()+
+  geom_vline(xintercept = WinterChrLines, color = 'black')+
+  geom_vline(xintercept = 4780, color = 'red')+
+  annotate(geom= 'text', x = 4780, y = 30, label = 'AlaAT1')+
+  geom_vline(xintercept = WinterChrLines)+
+  scale_x_continuous(label = c("1H","2H", "3H", "4H", "5H", "6H", "7H", "UN"),
+                     breaks = winterOrdinalBreaks)+
+  ylab('-log(p-value)')+xlab('Chromosome')+ geom_hline(yintercept = -log10(5e-5)) +
+  facet_grid(rows = vars(trait), scales = 'free_y')+theme_bw()
