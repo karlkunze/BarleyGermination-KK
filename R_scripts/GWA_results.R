fce1472db67755e914ddac302ee1a6e35a6dad1b
karlkunze <khk44@cornell.edu>
2022-02-25 16:21:15 -0500

    made commits for tables

--- R_scripts/GWA_results.R
+++ R_scripts/GWA_results.R
@@ -5,8 +5,13 @@ library(tibble);library(patchwork);library(ggplot2);library(fda) ; library(magic
 library(drc);library(rrBLUP);library(tidyr);library(ggh4x);library(dplyr)
 library(tidyr)
 load("data/GWA_results/WinterPerTPGWAS.Rdata")
-load("data/GWA_results/DH2020.GElogfitGWA_mlmm.Rdata")
 
+load("data/GWA_results/DH2020.GElogfitGWA_mlmm.Rdata")
+load("data/GWA_results/DH2021.GElogfitGWA_mlmm.Rdata")
+load("data/GWA_results/DHCombined.GElogfitGWA_mlmm.Rdata")
+
+load("data/GWA_results/DH2020_GIlogfitGWA_mlmm.Rdata")#
+load("data/GWA_results/DH2021_GIlogfitGWA_mlmm.Rdata")
 load("data/GWA_results/DH20202021_GIlogfitGWA_mlmm.Rdata")
 
 Add_DH130910_familyStructure = function(df3, groupvar){
@@ -34,14 +39,24 @@ for (i in 2:8){
 
 #View(DH2020Estimates %>% rbind(DH2021Estimates, DHCombined) %>%  filter(type == 'BLUE') %>%
    #    ungroup() %>% group_by(year, TP, trait))
-table(DHCombined$TP,DHCombined$PM_date)
-WinterPerTPGWAS
+
+
 
 library(dplyr)
 WinterPerTPGWAS %>% arrange(P.value) %>% ungroup()%>% select(SNP, Chromosome, Position) %>% unique()
-WinterPerTPGWAS %>% arrange(P.value) %>% View()
-#(P.value<5e-6)
-test$PM_date
+WinterPerTPGWAS %>% arrange(P.value) 
+
+SigHitsSingle_TP=WinterPerTPGWAS %>% arrange(P.value) %>% filter(P.value<5e-6) %>% filter(maf>0.05) %>% ungroup()%>% select(SNP, Chromosome,Position,maf,P.value,year,TP,trait)%>%
+  mutate(most_significant_model=paste0(year,"_",TP,"_",trait))%>%select(SNP,Chromosome,Position,most_significant_model,year,maf,P.value)%>%group_by(SNP)%>% slice_min(n = 1, P.value)
+
+
+SigHitsSingle_TP_counts<-WinterPerTPGWAS%>%arrange(P.value)%>%filter(P.value<5e-6)%>%filter(maf>0.05) %>% ungroup()%>% select(SNP, Chromosome,Position,maf,P.value,year,TP,trait)%>%
+ mutate(model=paste0(year,"_",TP,"_",trait))%>%select(SNP,Chromosome,Position,model,year,maf,P.value)%>%group_by(SNP,Chromosome,Position)%>%dplyr::summarize(count=n(),.groups="keep")%>%ungroup()%>%select(SNP,count)
+
+SigHitsSingle_TP=SigHitsSingle_TP%>%join(SigHitsSingle_TP_counts,by="SNP")%>%arrange(Chromosome,Position)
+SigHitsSingle_TP
+
+SigHitsSingle_TP
 table(WinterPerTPGWAS$TP)
 WinterPerTPGWAS%>%mutate(PM_date=mapvalues(TP,from=c("TP1","TP1.5","TP2","TP2.5","TP3","TP3.5","TP4","TP5"),to=c(5,12,19,33,47,68,96,152)))%>%
   mutate(Time_Point=paste0(TP,"(",PM_date,")"))%>%
@@ -68,5 +83,36 @@ DH2020.GElogfitGWA.mlmm %>%ggplot(aes(ordinal, log10PVal, color = term))+geom_po
   ylab('-log(p-value)')+xlab('Chromosome')+ geom_hline(yintercept = -log10(5e-5)) +
   facet_grid(scales = 'free_y')+theme_bw()
 
+#Logistic
+Log2020_GE=DH2020.GElogfitGWA.mlmm%>%arrange(P.value)%>%filter(P.value<5e-6)%>%filter(maf>0.05) %>% ungroup()%>% select(SNP, Chromosome,Position,maf,P.value,year,term)%>%
+mutate(most_significant_model=paste0("Logistic GE ",year,"_",term))
+Log2021_GE=DH2021.GElogfitGWA.mlmm%>%arrange(P.value)%>%filter(P.value<5e-6)%>%filter(maf>0.05) %>% ungroup()%>% select(SNP, Chromosome,Position,maf,P.value,year,term)%>%
+  mutate(most_significant_model=paste0("Logistic GE ",year,"_",term))
+LogCombined_GE=DHCombined.GElogfitGWA.mlmm%>%arrange(P.value)%>%filter(P.value<5e-6)%>%filter(maf>0.05) %>% ungroup()%>% select(SNP, Chromosome,Position,maf,P.value,year,term)%>%
+  mutate(most_significant_model=paste0("Logistic GE ",year,"_",term))
+
+Log2020_GI=DH2020_GIlogfitGWA.mlmm%>%arrange(P.value)%>%filter(P.value<5e-6)%>%filter(maf>0.05) %>% ungroup()%>% select(SNP, Chromosome,Position,maf,P.value,year,term)%>%
+  mutate(most_significant_model=paste0("Logistic GI ",year,"_",term))
+Log2021_GI=DH2021_GIlogfitGWA.mlmm%>%arrange(P.value)%>%filter(P.value<5e-6)%>%filter(maf>0.05) %>% ungroup()%>% select(SNP, Chromosome,Position,maf,P.value,year,term)%>%
+  mutate(most_significant_model=paste0("Logistic GI ",year,"_",term))
+LogCombined_GI=DH20202021_GIlogfitGWA.mlmm%>%arrange(P.value)%>%filter(P.value<5e-6)%>%filter(maf>0.05) %>% ungroup()%>% select(SNP, Chromosome,Position,maf,P.value,year,term)%>%
+  mutate(most_significant_model=paste0("Logistic GI ",year,"_",term))
 
 
+
+DH2020.GIlogfitGWA.mlmm%>%arrange(P.value)%>%filter(P.value<5e-6)%>%filter(maf>0.05) %>% ungroup()%>% select(SNP, Chromosome,Position,maf,P.value,year,term)%>%
+  mutate(most_significant_model=paste0("Logistic ",year,"_",term))
+DH2021.GElogfitGWA.mlmm%>%arrange(P.value)%>%filter(P.value<5e-6)%>%filter(maf>0.05) %>% ungroup()%>% select(SNP, Chromosome,Position,maf,P.value,year,term)%>%
+  mutate(most_significant_model=paste0("Logistic ",year,"_",term))
+
+Log_hits_all=rbind(Log2020_GE,Log2021_GE,LogCombined_GE,Log2020_GI,Log2021_GI,LogCombined_GI)%>%select(-term)
+Log_hits_filterd=Log_hits_all%>%filter(!most_significant_model=="Upper")%>%group_by(SNP)%>% slice_min(n = 1, P.value)
+
+Log_hits_counts=Log_hits_all%>%select(SNP,Chromosome,Position,most_significant_model,maf,P.value)%>%group_by(SNP,Chromosome,Position)%>%dplyr::summarize(count=n(),.groups="keep")%>%ungroup()%>%select(SNP,count)
+Log_hits_filterd=Log_hits_filterd%>%join(Log_hits_counts,by="SNP")%>%arrange(Chromosome,Position)
+
+
+
+Total_hits=rbind(SigHitsSingle_TP,Log_hits_filterd)%>%arrange(Chromosome,Position)%>%mutate(maf=round(as.numeric(maf),5))
+View(Total_hits)
+write.csv(Total_hits,file = "data/GWA_results/Winter_SingleTP_andLogisticHits.csv")
