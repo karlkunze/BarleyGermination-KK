f83353af3005cdd4d52ab743c9987cf6feb5d3df
karlkunze <khk44@cornell.edu>
2022-06-01 13:02:51 -0400

    renamed sample number to plot

--- R_scripts/MQ/MQ_analysis.R
+++ R_scripts/MQ/MQ_analysis.R
@@ -1,1 +1,11 @@
 #analysis of MQ data
+
+load("data/MQ/WMB21_Moistures.Rdata")
+load("data/MQ/CM/WMB21_CM.Rdata")
+load("data/MQ/SWE/WMB21_SWE.Rdata")
+SWE
+library(readxl)
+library(dplyr)
+WMB21_leco<- read_excel("data/MQ/21CY Cornell Winter TB Malting Leco Final Report.xlsx",skip=2)%>%as.data.frame()%>%rename()
+
+names(WMB21_leco)
