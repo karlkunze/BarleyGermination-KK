5c5604001a5f1286aed442225d0d30376f2782ae
karlkunze <khk44@cornell.edu>
2022-06-01 11:51:08 -0400

    changes to selection

--- R_scripts/2022_selection.R
+++ R_scripts/2022_selection.R
@@ -3,6 +3,7 @@ load("data/Malting_quality/SWE/WMB21_CM.Rdata")
 load("data/Malting_quality/SWE/WMB21_SWE.Rdata")
 SWE
 library(readxl)
+library(dplyr)
 WMB22_Reg_seneca <- as.data.frame(read_excel("C:/Users/kars8/Dropbox/Cornell Small Grains Breeding/2022/Winter Grains22/WMB Repl22/WMB22- Reg_kk.xlsx", 
                         sheet = "Seneca Co."))%>%select(Entry,PLOT,ws_percent)%>%arrange(PLOT)
 
@@ -35,15 +36,94 @@ total$Row<-as.factor(total$Row)
 total$Column<-as.factor(total$Column)
 total$location<-as.factor(total$location)
 total$Entry<-as.factor(total$Entry)
-library(asreml)
 
-ws<-asreml(fixed = ws_percent~Entry + location ,random=~Entry:location,residual =~dsum(~ar1(Row):ar1(Column)|location),data = total)
-summary(ws)$varcomp
-model<-as.data.frame(predict(ws,classify = "Entry"))
+#per location
+table(total$location)
+loc_spec<-total%>%filter(location%in%c("Helfer"))
+wsl<-asreml(fixed = ws_percent~1 ,random=~Entry,residual = ~units,data = loc_spec)
+wsl2<-asreml(fixed = ws_percent~1 ,random=~Entry,residual = ~ar1(Row):ar1(Column),data = loc_spec)
+wsl20<-asreml(fixed = ws_percent~1 ,random=~Entry,residual = ~ar1(Row):id(Column),data = loc_spec)
+
+infoCriteria.asreml(wsl)
+infoCriteria.asreml(wsl2)
+infoCriteria.asreml(wsl20)
+
+model_l<-as.data.frame(predict(wsl,classify = "Entry"))
+ws_scores_helfer<-model_l%>%select(pvals.Entry,pvals.predicted.value)%>%dplyr::rename(Entry=1,ws=2)%>%arrange(desc(Entry))%>%as.data.frame()
+ws_scores_helfer$location<-"Helfer"
+
+#
+table(total$location)
+loc_spec<-total%>%filter(location%in%c("Snyder"))
+wsl<-asreml(fixed = ws_percent~1 ,random=~Entry,residual = ~units,data = loc_spec)
+wsl2<-asreml(fixed = ws_percent~1 ,random=~Entry,residual = ~ar1(Row):ar1(Column),data = loc_spec)
+wsl20<-asreml(fixed = ws_percent~1 ,random=~Entry,residual = ~ar1(Row):id(Column),data = loc_spec)
+wsl02<-asreml(fixed = ws_percent~1 ,random=~Entry,residual = ~id(Row):ar1(Column),data = loc_spec)
+
+infoCriteria.asreml(wsl)
+infoCriteria.asreml(wsl2)
+infoCriteria.asreml(wsl20)
+infoCriteria.asreml(wsl02)
+
+model_l<-as.data.frame(predict(wsl02,classify = "Entry"))
+ws_scores_snyder<-model_l%>%select(pvals.Entry,pvals.predicted.value)%>%dplyr::rename(Entry=1,ws=2)%>%arrange(desc(Entry))%>%as.data.frame()
+ws_scores_snyder$location<-"Snyder"
+##
+loc_spec<-total%>%filter(location%in%c("Seneca"))
+wsl<-asreml(fixed = ws_percent~1 ,random=~Entry,residual = ~units,data = loc_spec)
+wsl2<-asreml(fixed = ws_percent~1 ,random=~Entry,residual = ~ar1(Row):ar1(Column),data = loc_spec)
+wsl20<-asreml(fixed = ws_percent~1 ,random=~Entry,residual = ~ar1(Row):id(Column),data = loc_spec)
+wsl02<-asreml(fixed = ws_percent~1 ,random=~Entry,residual = ~id(Row):ar1(Column),data = loc_spec)
+
+infoCriteria.asreml(wsl)
+infoCriteria.asreml(wsl2)
+infoCriteria.asreml(wsl20)
+infoCriteria.asreml(wsl02)
+
+model_l<-as.data.frame(predict(wsl02,classify = "Entry"))
+ws_scores_seneca<-model_l%>%select(pvals.Entry,pvals.predicted.value)%>%dplyr::rename(Entry=1,ws=2)%>%arrange(desc(Entry))%>%as.data.frame()
+ws_scores_seneca$location<-"Seneca"
+
+##
+total$location
+loc_spec<-total%>%filter(location%in%c("Livingston"))
+loc_spec
+wsl<-asreml(fixed = ws_percent~1 ,random=~Entry,residual = ~units,data = loc_spec)
+wsl2<-asreml(fixed = ws_percent~1 ,random=~Entry,residual = ~ar1(Row):ar1(Column),data = loc_spec)
+wsl20<-asreml(fixed = ws_percent~1 ,random=~Entry,residual = ~ar1(Row):id(Column),data = loc_spec)
+wsl02<-asreml(fixed = ws_percent~1 ,random=~Entry,residual = ~id(Row):ar1(Column),data = loc_spec)
+
+infoCriteria.asreml(wsl)
+infoCriteria.asreml(wsl2)
+infoCriteria.asreml(wsl20)
+infoCriteria.asreml(wsl02)
+
+model_l<-as.data.frame(predict(wsl02,classify = "Entry"))
+ws_scores_avon<-model_l%>%select(pvals.Entry,pvals.predicted.value)%>%dplyr::rename(Entry=1,ws=2)%>%arrange(desc(Entry))%>%as.data.frame()
+ws_scores_avon$location<-"Livingston"
+ws_scores_avon
+
+ws_per_env<-plyr::rbind.fill(ws_scores_helfer,ws_scores_seneca,ws_scores_avon,ws_scores_snyder)
+ws_per_env<-ws_per_env%>%tidyr::pivot_wider(values_from = "ws",names_from = "location")
+
+library(asreml)
+library(asremlPlus)
+ws<-asreml(fixed = ws_percent~ location ,random=~Entry+location,residual = ~units,data = total)
+
+
+ws1<-asreml(fixed = ws_percent~ location ,random=~Entry+location,residual =~dsum(~id(Row):ar1(Column)+id(Row):id(Column)|location,levels=list(c(2,3,4),c(1))),data = total)
+ws1
+infoCriteria.asreml(ws)
+infoCriteria.asreml(ws1)
+model<-as.data.frame(predict(ws1,classify = "Entry"))
 model
 ws
 ws_scores<-model%>%select(pvals.Entry,pvals.predicted.value)%>%dplyr::rename(Entry=1,ws=2)%>%arrange(desc(Entry))
 View(ws_scores)
+ws_scores<-ws_scores%>%rename(all_locations=ws)
+ws_per_env<-ws_per_env%>%full_join(ws_scores,by="Entry")
+View(ws_per_env)
+write.csv(ws_per_env,file="C:/Users/kars8/Dropbox/Cornell Small Grains Breeding/2022/Winter Grains22/ws_per_env.csv")
 DH<-ws_scores %>%filter(grepl('BS', Entry))%>%filter(ws>87)
 DH
 CM$ME<-round(as.numeric(CM$ME),2)  
